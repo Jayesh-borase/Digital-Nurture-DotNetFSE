e8e044b9bcc1ab0c350cc05794f00ef3c07f4c85
Jayeshborse18 <borsejayesh069@gmail.com>
2026-06-28 17:56:50 +0530

    Add files via upload

---
+++ week2/Lab2/AppDbContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+using RetailInventory.Models;
+
+namespace RetailInventory.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(
+                "Server=LAPTOP-KCCF6VG8\\SQLEXPRESS;Database=RetailInventoryDB;Trusted_Connection=True;TrustServerCertificate=True;"
+            );
+        }
+    }
+}

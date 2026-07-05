cb82287e15d66515b151f6bca6d5579d654555b5
jayesh-borase <borsejayesh069@gmail.com>
2026-07-05 17:20:11 +0530

    Lab2 Files

---
+++ week3/lab2/SwaggerDemoApi/Controllers/EmployeeController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FirstWebApi.Models;
+
+namespace FirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("api/Emp")]   
+    public class EmployeeController : ControllerBase
+    {
+        private static readonly List<Employee> employees =
+        [
+            new Employee
+            {
+                Id = 1,
+                Name = "Ajay",
+                Department = "IT",
+                Salary = 50000
+            },
+
+            new Employee
+            {
+                Id = 2,
+                Name = "Rahul",
+                Department = "HR",
+                Salary = 45000
+            }
+        ];
+
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<Employee>> GetEmployees()
+        {
+            return Ok(employees);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public ActionResult<Employee> AddEmployee(Employee employee)
+        {
+            employees.Add(employee);
+
+            return Created("", employee);
+        }
+    }
+}
